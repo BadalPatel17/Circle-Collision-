04d0d498d49a07e758a38ec5ee7045369fde45b4
BadalPatel17 <124709920+BadalPatel17@users.noreply.github.com>
2023-12-18 23:34:09 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,111 @@
+let canvas = document.getElementById('game-canvas');
+let ctx = canvas.getContext('2d');
+canvas.width = 800;
+canvas.height = 800;
+
+
+let player = {
+  x: canvas.width,
+  y: canvas.height,
+  radius: 10,
+  color: '#3498db',
+};
+
+
+let foods = [];
+let foodSize = 10;
+let growthFactor = 1 / 8;
+let foodInterval = 2000; // milliseconds
+
+
+// Generate random number between min and max
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+// Create random food circle
+function createFood() {
+  let food = {
+    x: getRandomNumber(foodSize, canvas.width - foodSize),
+    y: getRandomNumber(foodSize, canvas.height - foodSize),
+    radius: foodSize,
+    color: '#e74c3c',
+  };
+  foods.push(food);
+}
+
+
+// Draw player on the canvas
+function drawPlayer() {
+  ctx.beginPath();
+  ctx.arc(player.x, player.y, player.radius, 0, Math.PI * 2);
+  ctx.fillStyle = player.color;
+  ctx.fill();
+  ctx.closePath();
+}
+
+
+// Draw food circles on the canvas
+function drawFoods() {
+  foods.forEach(food => {
+    ctx.beginPath();
+    ctx.arc(food.x, food.y, food.radius, 0, Math.PI * 2);
+    ctx.fillStyle = food.color;
+    ctx.fill();
+    ctx.closePath();
+  });
+}
+
+
+// Update player position to follow the mouse
+function updatePlayerPosition(event) {
+  player.x = event.clientX - canvas.getBoundingClientRect().left;
+  player.y = event.clientY - canvas.getBoundingClientRect().top;
+}
+
+
+// Check if a point is inside a circle
+function pointInsideCircle(x, y, circle) {
+  let distance = Math.sqrt((x - circle.x) ** 2 + (y - circle.y) ** 2);
+  return distance <= circle.radius;
+}
+
+
+// Handle collisions with food
+function handleFoodCollisions() {
+  for (let i = foods.length - 1; i >= 0; i--) {
+    if (pointInsideCircle(player.x, player.y, foods[i])) {
+      player.radius += foods[i].radius * growthFactor;
+      foods.splice(i, 1);
+    }
+  }
+}
+
+
+// Game loop
+function gameLoop() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+
+  drawFoods();
+  drawPlayer();
+  handleFoodCollisions();
+
+
+  requestAnimationFrame(gameLoop);
+}
+
+
+// Initialize the game
+function init() {
+  canvas.addEventListener('mousemove', updatePlayerPosition);
+  setInterval(createFood, foodInterval);
+
+
+  gameLoop();
+}
+
+
+// Run the game
+init();
